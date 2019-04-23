405afa841e5300e46d392eab2950f92f3fb152f5
EC2 Default User <ec2-user@ip-172-31-22-110.ec2.internal>
2019-04-23 21:02:28 +0000

    fix: correct dask slicing to account for inclusive upper bound

--- generate.py
+++ generate.py
@@ -1,3 +1,4 @@
+from multiprocessing import cpu_count
 from sys import argv
 
 import dask.array as da
@@ -13,7 +14,7 @@ seed(0)
 def generate(n_nodes, edge_density=0.2):
     n_edges_max = n_nodes * n_nodes # allow self-loops
     n_edges = int(n_edges_max*edge_density)
-    edges = randint(0, n_nodes, size=(1.1*n_edges, 2))
+    edges = randint(0, n_nodes, size=(1.2*n_edges, 2))
     edges = dd.from_dask_array(
             edges,
             columns=['presyn_segid', 'postsyn_segid'])
@@ -23,10 +24,13 @@ def generate(n_nodes, edge_density=0.2):
             positions,
             columns = ['centroid_x', 'centroid_y', 'centroid_z'])
     positions = positions.drop_duplicates()
-    edges.repartition(npartitions=8)
-    positions.repartition(npartitions=8)
-    edges = edges.loc[:n_edges]
-    positions = positions.loc[:edges.shape[0]]
+    edges = edges.loc[:(n_edges-1)]
+    positions = positions.loc[:(edges.shape[0]-1)]
+    edges.repartition(npartitions=cpu_count())
+    positions.repartition(npartitions=cpu_count())
+    print(n_edges)
+    print(edges.shape[0].compute())
+    print(positions.shape[0].compute())
     df = dd.concat([edges, positions], axis=1)
     return df
 
