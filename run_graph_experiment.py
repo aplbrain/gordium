11867920368d669140caefe63e7922841fb67a3d
EC2 Default User <ec2-user@ip-172-31-31-243.ec2.internal>
2019-05-04 15:51:59 +0000

    fix: use perf_counter to measure performance

--- run_graph_experiment.py
+++ run_graph_experiment.py
@@ -2,7 +2,7 @@ from collections import defaultdict
 from math import sqrt
 from sys import argv
 from subprocess import run
-from time import process_time, sleep
+from time import perf_counter, sleep
 
 from dask.array.random import seed
 import dask.dataframe as dd
@@ -18,34 +18,34 @@ seed(0)
 
 if __name__ == '__main__':
     EDGE_DENSITY = 0.01
-    start_time = process_time()
+    start_time = perf_counter()
     neo4j_records = list()
     networkx_records = list()
     orders_of_magnitude = range(1, 10)
     for oom in orders_of_magnitude:
         record = dict()
-        record['start_time'] = process_time()
+        record['start_time'] = perf_counter()
         n_edges_desired = 10**oom
         n_nodes = int(1.1*sqrt(n_edges_desired/EDGE_DENSITY))
         edgeframe = generate(n_nodes, edge_density=EDGE_DENSITY)
         edgeframe.to_csv('/store/random/*.csv', index=False)
         if oom <= 7:
             edgeframe.to_csv('/home/ec2-user/gordium/edgeframe_{}/*.csv'.format(oom), index=False)
-        record['generate_time'] = process_time()
+        record['generate_time'] = perf_counter()
         edgeframe = None
         # run for Neo4j
         edgeframe = dd.read_csv('/store/random/*.csv')
-        record['read_time'] = process_time()
+        record['read_time'] = perf_counter()
         prepare_admin_import(edgeframe, '/store/import')
         edgeframe = None
-        record['convert_time'] = process_time()
+        record['convert_time'] = perf_counter()
         run('docker run -it --rm -v /store/data:/data -v /store/import:/import -v /home/ec2-user/gordium/scripts:/scripts --env NEO4J_AUTH=neo4j/neuprint --env NEO4J_dbms_memory_pagecache_size=50G --env NEO4J_dbms_memory_heap_max__size=750G -p 7474:7474 -p 7687:7687 --entrypoint /scripts/import_command.sh neo4j'.split())
-        record['import_time'] = process_time()
+        record['import_time'] = perf_counter()
         run('docker run --detach --rm -v /store/data:/data -v /home/ec2-user/gordium/plugins:/plugins -v /home/ec2-user/gordium/scripts:/scripts --env NEO4J_AUTH=neo4j/neuprint --env NEO4J_dbms_security_procedures_unrestricted=algo.\* --env NEO4J_dbms_memory_pagecache_size=50G --env NEO4J_dbms_memory_heap_max__size=750G -p 7474:7474 -p 7687:7687 --name neuprint-db neo4j'.split())
         sleep(20)
-        record['db_up_time'] = process_time()
+        record['db_up_time'] = perf_counter()
         run('docker exec -it neuprint-db /scripts/index_command.sh'.split())
-        record['index_time'] = process_time()
+        record['index_time'] = perf_counter()
         graph = Gordium(db_bolt_uri='bolt://localhost:7687')
         # so I actually needed to do this
         # because the connection pool seemed to get sour
@@ -56,20 +56,20 @@ if __name__ == '__main__':
             pass
         for fn in graph.fns:
             record[fn.__name__] = fn()
-            record['{}_time'.format(fn.__name__)] = process_time()
+            record['{}_time'.format(fn.__name__)] = perf_counter()
         run('docker kill neuprint-db'.split())
-        record['db_down_time'] = process_time()
+        record['db_down_time'] = perf_counter()
         run('docker run -it --rm -v /store/data:/data -v /store/import:/import -v /home/ec2-user/gordium/scripts:/scripts --entrypoint /scripts/clear_command.sh neo4j'.split())
         graph = None
-        record['end_time'] = process_time()
+        record['end_time'] = perf_counter()
         neo4j_records.append(record)
         neo4j_records_df = DataFrame(neo4j_records)
         neo4j_records_df.to_csv('graph_experiment_benchmarks_neo4j.csv', index=False)
         # run for NetworkX
         record = dict()
-        record['start_time'] = process_time()
+        record['start_time'] = perf_counter()
         edgeframe = dd.read_csv('/store/random/*.csv').compute()
-        record['read_time'] = process_time()
+        record['read_time'] = perf_counter()
         graph = from_pandas_edgelist(
                 edgeframe,
                 source='presyn_segid',
@@ -77,12 +77,12 @@ if __name__ == '__main__':
                 edge_attr=True,
                 create_using=DiGraph)
         edgeframe = None
-        record['convert_time'] = process_time()
+        record['convert_time'] = perf_counter()
         for fn in table_fns:
             record[fn.__name__] = fn(graph)
-            record['{}_time'.format(fn.__name__)] = process_time()
+            record['{}_time'.format(fn.__name__)] = perf_counter()
         graph = None
-        record['end_time'] = process_time()
+        record['end_time'] = perf_counter()
         networkx_records.append(record)
         networkx_records_df = DataFrame(networkx_records)
         networkx_records_df.to_csv('graph_experiment_benchmarks_networkx.csv', index=False)
