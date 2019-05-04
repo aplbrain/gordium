7e11b34400a9f84de7ddc4b19401b5ee934fc3fc
Joe Downs <joe.downs@jhuapl.edu>
2019-05-03 23:15:00 -0400

    refactor: rename degree histogram variables

--- gordium/_backend.py
+++ gordium/_backend.py
@@ -30,20 +30,20 @@ class GraphBackend(ABC):
         pass
 
     def number_of_leaves(self):
-        degree = self.degree_histogram()
-        return degree[1]
+        dh = self.degree_histogram()
+        return dh[1]
 
     def number_of_nodes_with_degree_over_1000(self):
-        degree = self.degree_histogram()
-        return degree[degree.index > 1000].sum()
+        dh = self.degree_histogram()
+        return dh[dh.index > 1000].sum()
 
     def max_degree(self):
-        degree = self.degree_histogram()
-        return degree.index.max()
+        dh = self.degree_histogram()
+        return dh.index.max()
 
     def mean_degree(self):
-        degree = self.degree_histogram()
-        return degree.dot(degree.index)/degree.sum()
+        dh = self.degree_histogram()
+        return dh.dot(dh.index)/dh.sum()
 
     @abstractmethod
     def scc_histogram(self):
